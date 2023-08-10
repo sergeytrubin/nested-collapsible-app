6cfc897b503031a18fe86b3194cc7d499691c079
st895028 <sergey.trubin@broadcom.com>
2023-08-10 15:33:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css';  // This will include our CSS styles
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Collapsible = ({ children, label }) => {
+    
+    const [IsOpen, setIsOpen] = useState(false);
+    
+    return (
+        <div className='container'>
+            <button className='toggle' onClick={() => setIsOpen(!IsOpen)}>{label}</button>
+            <div className={IsOpen ? 'content show' : 'content'}>{children}</div>
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="outer-container">
+            <Collapsible label='Click Here'>
+                <ul>
+                    <li>Profile</li>
+                    <li>Dashboard</li>
+                    <li>Logout</li>
+                </ul>
+            </Collapsible>
+        </div>
+    );
 }
 
 export default App;
+
